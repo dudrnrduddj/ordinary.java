86419b751ecaddd5569af2076a05adeb384ffb18
user1 <user1@DESKTOP-L4JUTT1>
2024-05-31 17:50:17 +0900

    [ADD] 자바 11일차 카페 키오스크 프로그램 - 미완

---
+++ src/org/ordinary/cafe/Item.java
@@ -0,0 +1,52 @@
+package org.ordinary.cafe;
+
+public class Item {
+		private String name;
+		private int price;
+		private int num;
+		
+		private int iceOrHot;
+		private int cupSize;
+		private int addShot;
+		
+		
+		
+		public Item(String name, int price, int num) {
+			this.name = name;
+			this.price = price;
+			this.num = num;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public int getPrice() {
+			return this.price;
+		}
+
+		public int getNum() {
+			return this.num;
+		}
+		
+		
+		public void setItemDetail(int iceOrHot, int cupSize, int addShot) {
+			this.iceOrHot = iceOrHot;
+			this.cupSize = cupSize;
+			this.addShot = addShot;
+		}
+		
+		public int getIceOrHot() {
+			return this.iceOrHot;
+		}
+
+		public int getCupSize() {
+			return cupSize;
+		}
+
+		public int getAddShot() {
+			return addShot;
+		}
+		
+		
+}
